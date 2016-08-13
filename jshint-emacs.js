cff30e7dcd29b3e57e4565f9fc9fb92fd7c0aa2c
Jean-Sebastien Legare <jslegare@cs.ubc.ca>
2016-08-12 17:22:34 -0700

    [lint] Long line split

--- jshint-emacs.js
+++ jshint-emacs.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-var conf = '\n(add-to-list \'load-path "' + __dirname + '")\n'
-  + '(require \'flymake-jshint)\n'
-  + '(add-hook \'javascript-mode-hook\n'
-  + '    (lambda () (flymake-mode t)))';
+var conf = '\n(add-to-list \'load-path "' + __dirname + '")\n' +
+  '(require \'flymake-jshint)\n' +
+  '(add-hook \'javascript-mode-hook\n' +
+  '(lambda () (flymake-mode t)))';
 
 console.log(conf);
